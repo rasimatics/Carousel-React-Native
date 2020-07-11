0d6e7c4cf003f7515dbb0602a7901fa990dce5e1
Rasim <rasimmammadoff1@gmail.com>
2020-07-12 01:33:26 +0400

    Menu added to card detail page

--- components/CardDetails.js
+++ components/CardDetails.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import { StyleSheet, View, Text, TouchableHighlight } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { StyleSheet, View, Text, TouchableOpacity, Dimensions, ScrollView, TouchableWithoutFeedback } from 'react-native';
+import Icon from 'react-native-vector-icons/Entypo';
+import Modal from 'react-native-modal';
 
 export default function CardDetails(props) {
-    return (
-        <View style={styles.container}>
-          <TouchableHighlight><Text>{props.route.params.id}</Text></TouchableHighlight>
-        </View >
-    );
+	const panelRef = useRef(null);
+
+	const [isModalVisible, setModalVisible] = useState(false);
+
+	return (
+
+		<View style={{ flex: 1 }}>
+			{/* Main content */}
+
+			<TouchableOpacity style={styles.menu} onPress={() => setModalVisible(true)}>
+				<Icon name="menu" size={25} color="#fff" />
+			</TouchableOpacity>
+
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-    container:{
-      flex:1,
-      marginTop:50,
-    },
+	container: {
+		flex: 1,
+		marginTop: 50,
+	},
+	menu: {
+		padding: 10,
+		backgroundColor: '#21254D',
+		width: 45,
+		borderRadius: 30,
+		position: 'absolute',
+		right: 12,
+		bottom: 12,
+	}
 });
